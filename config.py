b919eaae4792d06ae8293050141d434bb829bc3a
ishwaribangale <133501937+ishwaribangale@users.noreply.github.com>
2026-01-19 14:26:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+# ----------------------------------
+# Global Config
+# ----------------------------------
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    )
+}
+
+TIMEOUT = 12
+
+TOP_COMPANIES = [
+    {"name": "Notion", "ats": "lever", "slug": "notion"},
+    {"name": "Figma", "ats": "lever", "slug": "figma"},
+    {"name": "Airtable", "ats": "lever", "slug": "airtable"},
+
+    {"name": "Stripe", "ats": "greenhouse", "slug": "stripeinc"},
+    {"name": "Razorpay", "ats": "greenhouse", "slug": "razorpaysoftware"},
+    {"name": "Atlassian", "ats": "greenhouse", "slug": "atlassian"},
+    {"name": "PhonePe", "ats": "greenhouse", "slug": "phonepejobs"},
+]
